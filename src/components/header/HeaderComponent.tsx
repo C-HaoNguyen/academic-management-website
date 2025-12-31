19a75978852fc0c33c4eb4c11ec6dc2decffd475
Hao Nguyen <haolq0711@gmail.com>
2025-12-31 10:28:06 +0700

    Config react, tailwind

---
+++ src/components/header/HeaderComponent.tsx
@@ -0,0 +1,10 @@
+export default function HeaderComponent() {
+    return (
+        <header className="bg-blue-600 text-white p-4">
+            <div className="text-xl font-bold">
+                Ademy
+            </div>
+            <h1 className="text-2xl font-bold">Academic Management System</h1>
+        </header>
+    );
+}
